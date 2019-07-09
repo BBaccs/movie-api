f2fe8791f0940f4f43025156818b2b5b1e0620ca
Brian <baccarellab@gmail.com>
2019-07-09 12:18:14 -0400

    init

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // Init Open Movie Database api class
 class Omdbapi {
   constructor(){
-      this.api_key = 'thewdb';
+      this.api_key = '7ee80466a9a73a706bb3da43d231787f';
   }
 
   // Fetch movies
   async getMovie(movieTitle) {
-      const moviesResponse = await fetch(`https://www.omdbapi.com/?S=${movieTitle}&apikey=${this.api_key}`);
+      const moviesResponse = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${this.api_key}&movie_id='${movieTitle}'`);
+      console.log(moviesResponse)
       //json returns a promise that resolves with the result of parsing the moviesResponse
       const movies = await moviesResponse.json();
       return movies;
   }
 }
+
+
+
+//API Read Access Token (v4 auth)
+/*eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWU4MDQ2NmE5YTczYTcwNmJiM2RhNDNkMjMxNzg3ZiIsInN1YiI6IjVkMjRiYTNiNmQ0Yzk3MDAxMDdiZjVlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3th3HJY4erxGWXK223_3toUklzE3Nsl7ySDjifOGMcg*/
